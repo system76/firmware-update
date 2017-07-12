0e16088edef149967eaa5641c99ff60ebf058215
Jeremy Soller <jackpot51@gmail.com>
2017-07-12 15:06:53 -0600

    Fix issue with uninstall.sh not removing /boot/efi/system76-fu

--- uninstall.sh
+++ uninstall.sh
@@ -1,22 +1,34 @@
 #!/bin/bash
 set -e
 
+if [ "$EUID" != "0" ]
+then
+    echo "You are not running as root" >&2
+    exit 1
+fi
+
 if [ -e /boot/efi/system76-fu ]
 then
     echo -e "\e[1mRemoving /boot/efi/system76-fu\e[0m" >&2
-    sudo rm -rfv /boot/efi/system76-fu
+    rm -rfv /boot/efi/system76-fu
+else
+    echo -e "\e[1mAlready removed /boot/efi/system76-fu\e[0m" >&2
 fi
 
-if [ -n "$(sudo efibootmgr | grep '^BootNext')" ]
+if [ -n "$(efibootmgr | grep '^BootNext: 1776$')" ]
 then
     echo -e "\e[1mUnsetting BootNext\e[0m" >&2
-    sudo efibootmgr -N
+    efibootmgr -N
+else
+    echo -e "\e[1mAlready unset BootNext\e[0m" >&2
 fi
 
-for label in $(sudo efibootmgr | grep '^Boot[0-9]\{4\}\* System76 Firmware Flasher' | cut -d '*' -f1 | sed 's/Boot//')
-do
-    echo -e "\e[1mRemoving Boot$label\e[0m" >&2
-    sudo efibootmgr -b "$label" -B
-done
+if [ -n "$(efibootmgr | grep '^Boot1776\* ')" ]
+then
+    echo -e "\e[1mRemoving Boot1776\e[0m" >&2
+    efibootmgr -B -b 1776
+else
+    echo -e "\e[1mAlready removed Boot1776\e[0m" >&2
+fi
 
 echo -e "\e[1mUninstalled system76-fu\e[0m" >&2
