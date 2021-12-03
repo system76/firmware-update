daf5d5074cfaf67a6355c09fe7c4482e618d3c60
Jeremy Soller <jeremy@system76.com>
2021-12-03 10:08:12 -0700

    Add galp5 3050 EC project

--- src/app/ec.rs
+++ src/app/ec.rs
@@ -164,7 +164,7 @@ impl EcComponent {
                     }
                 },
                 "NS50MU" => "system76/darp7".to_string(),
-                "NV40Mx" | "NV40Mx-DV" => "system76/galp5".to_string(),
+                "NV40Mx" | "NV40Mx-DV" | "NV40MJ" => "system76/galp5".to_string(),
                 "PB50Ex" => "system76/addw1".to_string(),
                 "PBx0Dx2" => "system76/addw2".to_string(),
                 "P950Ex" => "system76/oryp5".to_string(),
